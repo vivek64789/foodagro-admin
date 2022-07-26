5ce61709d9c2a119925198d27da21ec33941166b
vivek64789 <anandkushwaha2074@gmail.com>
2022-07-26 22:39:22 +0545

    final

---
+++ src/Utils/server.js
@@ -0,0 +1,5 @@
+const logo = "https://firebasestorage.googleapis.com/v0/b/foodagro-adminpanel.appspot.com/o/logo.png?alt=media&token=74293d44-6e98-4ac8-9504-6ae0119d48c4";
+// const serverUrl = "http://127.0.0.1:8080";
+const serverUrl = "https://foodagro-test-api.herokuapp.com";
+
+export { serverUrl, logo };
